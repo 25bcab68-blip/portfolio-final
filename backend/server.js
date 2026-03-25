053d429196c99434fad5af89838f8f35af0b754d
25bcab68-blip <25bcab68@kristujayanti.com>
2026-03-25 13:41:46 +0530

    init commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const Contact = require("./models/Contact");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// routes
+app.post("/api/contact", async (req, res) => {
+  try {
+    const newContact = new Contact(req.body);
+    await newContact.save();
+
+    res.json({ message: "Message saved successfully 🚀" });
+  } catch (err) {
+    res.status(500).json({ message: "Error saving message" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
